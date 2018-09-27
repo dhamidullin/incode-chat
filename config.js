f2a674da9350bf3a913148041e5d5787e855ab5b
dhamidullin <null>
2018-09-27 13:44:42 +0300

    changed db uri

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var socketioJwt = require("socketio-jwt");
 var http_port = 8080;
 var socket_port = 8081;
 
-var db_uri = "mongodb://localhost:27017/incode-chat";
+var db_uri = "mongodb://localhost:27017/sp-chat";
 
 var server_secret = "G1zIDwcI01z18gILRtta0LjgIEFjp7zu";
 var jwt_secret = "1NTMzxvwhx5MAJYMBpkgyXzsp8WmLeUi";
